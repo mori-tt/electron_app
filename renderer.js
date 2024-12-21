509968260a4598fc055340c748ef9dd6d88f4368
T_Mori <morit0301@gmail.com>
2024-12-22 04:27:53 +0900

    Frontend implementation

--- renderer.js
+++ renderer.js
@@ -5,3 +5,38 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+const $btnGet = document.querySelector("#btn-get");
+const $btnSave = document.querySelector("#btn-save");
+const $inputUrl = document.querySelector("#input-url");
+const $msg = document.querySelector("#msg");
+const $result = document.querySelector("#result");
+
+$btnGet.addEventListener("click", () => {
+  const targetUrl = $inputUrl.value;
+
+  // ブラウザ->画像URL一覧を取得
+  const imgUrls = [
+    "http://localhost:3000/villa-1.jpg",
+    "http://localhost:3000/villa-2.jpg",
+  ];
+
+  let imgHtmlStr = "";
+
+  for (const url of imgUrls) {
+    imgHtmlStr += `<img src="${url}"/>`;
+  }
+
+  $result.innerHTML = imgHtmlStr;
+});
+
+$btnSave.addEventListener("click", () => {
+  // 画像の保存の処理
+  const result = "success"; //"failed", "cancell"
+  const MSGs = {
+    success: "画像の保存に成功しました",
+    failed: "画像の保存に失敗しました",
+    cancel: "画像の保存を中断しました",
+  };
+  $msg.textContent = MSGs[result];
+});
